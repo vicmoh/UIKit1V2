d7b8b6c5bdfc48c5dc4054f892e66ea4987898f1
Vicky Mohammad <vixtinction@icloud.com>
2016-12-26 21:15:34 -0500

    Initial Commit

---
+++ UIKit1V2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIKit1V2
+//
+//  Created by Vicky Mohammad on 2016-12-26.
+//  Copyright © 2016 Vicky Mohammad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
