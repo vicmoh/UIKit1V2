e46bcb97a695cfdfb7c5623f6e666e7d3c9f81bf
Vicky Mohammad <vimoh1213@gmail.com>
2017-12-09 21:11:48 -0500

    jkl

--- UIKit1V2/ViewController.swift
+++ UIKit1V2/ViewController.swift
@@ -9,17 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //var
+    var count = 0
+    @IBOutlet var Label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+    }//end view did load
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
+    }//end func
 
+    @IBAction func Increment() {
+        self.count = count + 1
+        self.Label.text = "\(self.count)"
+    }//end func
+    
+    
+}//end class
 
-}
 
